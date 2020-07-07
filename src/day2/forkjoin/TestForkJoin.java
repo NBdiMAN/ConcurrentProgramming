3cc920e105be4be0d1ba388478955b84ffa09ff3
NBdiMAN <907645377@qq.com>
2020-07-07 21:49:20 +0800

    ForkJoin框架

--- src/day2/forkjoin/TestForkJoin.java
+++ src/day2/forkjoin/TestForkJoin.java
@@ -18,12 +18,14 @@ public class TestForkJoin extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if(toIndex - fromIndex < THRESHOLD){
+            System.out.println("执行。。。。。");
             int result = 0;
             for (int i = fromIndex; i <= toIndex; i++) {
                 result += src[i];
             }
             return result;
         }else{
+            System.out.println("拆分子任务");
             int mid = (toIndex + fromIndex) >> 1;
             TestForkJoin left = new TestForkJoin(src, fromIndex, mid);
             TestForkJoin right = new TestForkJoin(src, mid + 1, toIndex);
@@ -35,6 +37,7 @@ public class TestForkJoin extends RecursiveTask<Integer> {
 
     public static void main(String[] args) {
         ForkJoinPool pool = new ForkJoinPool();
+        //同步调用
         Integer result = pool.invoke(new TestForkJoin(MakeArray.getArray(), 0, MakeArray.LENGTH - 1));
         System.out.println(result);
     }
